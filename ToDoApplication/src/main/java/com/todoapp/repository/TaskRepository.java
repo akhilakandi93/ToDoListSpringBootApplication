30ca2cd2a02b1da9d7356caca5fb9e2e82ca1b18
Akhila Kandi <akandi7227@gmail.com>
2018-09-05 14:50:03 -0400

    commit

--- ToDoApplication/src/main/java/com/todoapp/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todoapp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.todoapp.model.TaskModel;
-
-public interface TaskRepository extends CrudRepository<TaskModel, Long>{
-
-}
